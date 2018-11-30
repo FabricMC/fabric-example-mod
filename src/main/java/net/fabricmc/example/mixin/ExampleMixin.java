4d0c364880fecb64ac6a3597fd426d42ef257701
Adrian Siekierka <kontakt@asie.pl>
2018-11-30 16:54:05 +0100

    update to 48b, new fabric-loader definition

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,12 +1,12 @@
 package net.fabricmc.example.mixin;
 
-import net.minecraft.client.MinecraftGame;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftGame.class)
+@Mixin(MinecraftClient.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
