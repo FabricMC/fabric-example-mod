6d8b630d66bda4579ca77af4d58ccaf8d76546d7
Will <supersaiyansubtlety@gmail.com>
2020-05-25 18:39:57 -0700

    Shouldn't use HEAD for constructors

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
+	@Inject(at = @At("RETURN"), method = "init()V")
 	private void init(CallbackInfo info) {
 		System.out.println("This line is printed by an example mod mixin!");
 	}
