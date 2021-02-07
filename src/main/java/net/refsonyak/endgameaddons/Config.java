b3b67c8c321fb87427154faa6140422a241af221
yakyakyakyak <jethro.rosette@outlook.com>
2021-02-07 21:44:44 +1100

    Renamed folders Created Config class

---
+++ src/main/java/net/refsonyak/endgameaddons/Config.java
@@ -0,0 +1,5 @@
+public class Config {
+    /*Class that contains all configurable variables for quick'n'easy balance changes
+      To use, simply create a class in here, give it some variables, import it into your
+      file. Call on it using Config.<yourclass>.<yourvariable> and voila!*/
+}
