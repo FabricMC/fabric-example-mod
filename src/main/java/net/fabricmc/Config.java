629e71f220b76281516c805b38d27902ecc5ed4f
yakyakyakyak <jethro.rosette@outlook.com>
2021-02-07 16:28:45 +1100

    Started looking into coding dragon armor w/ elytra

---
+++ src/main/java/net/fabricmc/Config.java
@@ -0,0 +1,6 @@
+public class config {
+    public static class DragonArmor {
+        public static final int[] BASE_DURABILITY = new int[] {17, 19, 20, 15};
+        private static final int[] PROTECTION_VALUES = new int[] {5, 8, 10, 5};
+    }
+}
