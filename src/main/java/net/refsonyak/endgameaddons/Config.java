073f66402c8d5628504473469905ea388f9d91f5
yakyakyakyak <jethro.rosette@outlook.com>
2021-02-07 21:49:01 +1100

    added config example

--- src/main/java/net/refsonyak/endgameaddons/Config.java
+++ src/main/java/net/refsonyak/endgameaddons/Config.java
@@ -2,4 +2,8 @@ public class Config {
     /*Class that contains all configurable variables for quick'n'easy balance changes
       To use, simply create a class in here, give it some variables, import it into your
       file. Call on it using Config.<yourclass>.<yourvariable> and voila!*/
+
+    public class dropRates {
+        public static final double dropRate = 1;
+    }
 }
