a98ab6bf9935ecf5f15b86b4acf5169bf6666011
rizet <ryzenupp@hotmail.com>
2021-07-03 18:33:27 -0400

    Add a clientside mod example

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -2,7 +2,7 @@ package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class ExampleMod implements ModInitializer, ClientModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -10,5 +10,14 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		System.out.println("Hello Fabric world!");
+	}
+	
+	@Override
+	public void onInitializeClient() {
+		// This code runs on the client side only, and should be used to set up
+		// client-side logic, such as render or integrated-server tweaks.
+		// Proceed with mild caution.
+		
+		System.out.println("Hello client world!");
 	}
 }
