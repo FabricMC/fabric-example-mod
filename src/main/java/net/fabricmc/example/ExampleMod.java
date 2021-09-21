962b5dedbb37f15a10d5f34d579bd9ed0041de31
StoneLabs <levyehrstein@googlemail.com>
2021-09-22 00:10:01 +0200

    Add name 'modid' to logger

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -5,7 +5,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ExampleMod implements ModInitializer {
-	public static final Logger LOGGER = LogManager.getLogger();
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LogManager.getLogger("modid");
 
 	@Override
 	public void onInitialize() {
