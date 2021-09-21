cca1e7dab3f0b45c9f338ac369ebdfb2cdf9977e
StoneLabs <levyehrstein@googlemail.com>
2021-09-21 14:03:49 +0200

    Refactor Logger calls

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -14,6 +14,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.log(Level.INFO, "Hello Fabric world!");
+		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -12,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.log(Level.INFO, "This line is printed by an example mod mixin!");
+		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
