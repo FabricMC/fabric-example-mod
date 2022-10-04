b15391d25e2e68906b58beebecf93c3fec251401
Ian_Cooper <ian.cooper@usm.cl>
2022-10-04 17:36:30 -0300

    Set workspace

--- src/main/java/net/ianCooper/tutorialmod/TutorialMod.java
+++ src/main/java/net/ianCooper/tutorialmod/TutorialMod.java
@@ -1,10 +1,10 @@
-package net.fabricmc.example;
+package net.ianCooper.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class TutorialMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
--- src/main/java/net/ianCooper/tutorialmod/mixin/ExampleMixin.java
+++ src/main/java/net/ianCooper/tutorialmod/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.ianCooper.tutorialmod.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.ianCooper.tutorialmod.TutorialMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		TutorialMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
