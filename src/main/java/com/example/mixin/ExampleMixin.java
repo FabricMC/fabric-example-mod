cead09f7fea9907bb7b9a64dbc6da2088dbd8454
George Soryal <79430767+GeorgeSoryal@users.noreply.github.com>
2023-06-10 07:42:41 -0400

    Update ExampleMixin.java
    fixed typo
    noticed an out of place V at end of a comment

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -10,6 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+		// This code is injected into the start of MinecraftServer.loadWorld()
 	}
 }
