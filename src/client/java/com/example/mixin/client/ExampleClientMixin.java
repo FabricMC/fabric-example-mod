db63c19dc60a16078bc68f1790de4b00e88b5282
mrjasonn <120269221+mrjasonn@users.noreply.github.com>
2024-03-11 23:43:25 +0800

    Update ExampleClientMixin.java

--- src/client/java/com/example/mixin/client/ExampleClientMixin.java
+++ src/client/java/com/example/mixin/client/ExampleClientMixin.java
@@ -10,6 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleClientMixin {
 	@Inject(at = @At("HEAD"), method = "run")
 	private void run(CallbackInfo info) {
-		// This code is injected into the start of MinecraftClient.run()V
+		// This code is injected into the start of MinecraftClient.run()
 	}
 }
