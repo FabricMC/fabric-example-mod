5f7cd8dee00824795086f4535668eef4d3eda7c3
JaaiDead <98309228+JaaiDead@users.noreply.github.com>
2024-07-22 14:20:43 +0530

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -9,7 +9,8 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    	public static final String MOD_ID = "modid";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
